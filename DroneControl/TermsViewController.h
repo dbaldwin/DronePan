254c1e14b0f5a5c125aad4f7c559387aaa102993
Dennis Baldwin <dennisbaldwin@gmail.com>
2015-08-30 21:39:38 -0500

    Refactoring background API calls. Adding toast messages. Adding terms.

---
+++ DroneControl/TermsViewController.h
@@ -0,0 +1,13 @@
+//
+//  TermsViewController.h
+//  DronePan
+//
+//  Created by Dennis Baldwin on 8/24/15.
+//  Copyright (c) 2015 Unmanned Airlines, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TermsViewController : UIViewController
+
+@end
