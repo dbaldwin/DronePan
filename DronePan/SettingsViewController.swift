e3aca4ab15bbcaa46b98b2b279489739464bdf7d
Dennis Baldwin <dennisbaldwin@gmail.com>
2016-04-17 22:23:29 -0500

    More settings work

--- DronePan/SettingsViewController.swift
+++ DronePan/SettingsViewController.swift
@@ -18,8 +18,11 @@ import UIKit
 @objc class SettingsViewController: UIViewController {
     
     var model:String = ""
-    var photoCount:Int = 6
+    var photoCount:Int = 6 // Number of photos in a row
+    var startDelay:Int = 5 // Number of seconds to delay before pano
     var defaults = NSUserDefaults.standardUserDefaults()
+    
+    @IBOutlet weak var startDelayControl: UISegmentedControl!
     
     @IBOutlet weak var photoDelayControl: UISegmentedControl!
     
@@ -35,16 +38,47 @@ import UIKit
     
     func initSettings() {
         
-        let photos_per_row = defaults.objectForKey(model)!.valueForKey("photos_per_row")
+        // Number of seconds to delay before pano starts
+        var start_delay = 5
+        
+        if let saved_start_delay = defaults.objectForKey(model)?.valueForKey("start_delay") as? Int {
+            start_delay = saved_start_delay
+        }
+        
+        print(start_delay)
+        
         
         // Set the selected item
-        for i in 0...photosPerRowControl.numberOfSegments-1 {
+        for i in 0..<startDelayControl.numberOfSegments {
             
-            if(photosPerRowControl.titleForSegmentAtIndex(i) == photos_per_row?.stringValue) {
-                photosPerRowControl.selectedSegmentIndex = i
-                break
+            if let title = startDelayControl.titleForSegmentAtIndex(i) {
+                if let segment = Int(title) {
+                    if (segment == start_delay) {
+                        startDelayControl.selectedSegmentIndex = Int(i)
+                    }
+                }
             }
         }
+        
+        // Define the number of photos in a row
+        var photos_per_row = 6
+        
+        if let saved_per_row = defaults.objectForKey(model)?.valueForKey("photos_per_row") as? Int {
+            photos_per_row = saved_per_row
+        }
+        
+        // Set the selected item
+        for i in 0..<photosPerRowControl.numberOfSegments {
+            
+            if let title = photosPerRowControl.titleForSegmentAtIndex(i) {
+                if let segment = Int(title) {
+                    if (segment == photos_per_row) {
+                        photosPerRowControl.selectedSegmentIndex = Int(i)
+                    }
+                }
+            }
+        }
+        
     }
     
     @IBAction func photosPerRowChanged(sender: AnyObject) {
@@ -57,16 +91,21 @@ import UIKit
     
     @IBAction func saveSettings(sender: AnyObject) {
         
-        // Dismiss the VC
-        self.dismissViewControllerAnimated(true, completion: {})
+        startDelay = Int(startDelayControl.titleForSegmentAtIndex(startDelayControl.selectedSegmentIndex)!)!
+        
+        photoCount = Int(photosPerRowControl.titleForSegmentAtIndex(photosPerRowControl.selectedSegmentIndex)!)!
         
         let settings : [String : Int] = [
+            "start_delay" : startDelay,
             "photos_per_row" : photoCount
             /* add other settings in here */
         ]
         
         defaults.setObject(settings, forKey: model)
         defaults.synchronize()
+        
+        // Dismiss the VC
+        self.dismissViewControllerAnimated(true, completion: {})
     }
     
     func getSetting(setting: String) -> Int{
