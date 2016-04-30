d08c6183d6db4d92590fb780619d1a7163c753ad
Chris Searle <chris@chrissearle.org>
2016-04-30 20:07:55 +0200

    Try a separate scheme to split test runs to see if that prevents issues with simulator timeouts on travis

--- scripts/test-coverage.sh
+++ scripts/test-coverage.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-xctool -scheme DronePan clean build test -sdk iphonesimulator9.3 -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.3' GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES
+set -o pipefail && xcodebuild -scheme DronePan -sdk iphonesimulator9.3 -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.3' clean build GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES | xcpretty
+sleep 10
+set -o pipefail && xcodebuild -scheme DronePan -sdk iphonesimulator9.3 -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.3' test GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES | xcpretty
+sleep 10
+set -o pipefail && xcodebuild -scheme DronePanUITests -sdk iphonesimulator9.3 -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.3' test GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES | xcpretty
