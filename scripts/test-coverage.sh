06832127464072d36999c263d72681540f9b56cc
Chris Searle <chris@chrissearle.org>
2016-05-11 18:21:17 +0200

    Travis travis travis

--- scripts/test-coverage.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-set -o pipefail && xcodebuild -scheme DronePan -sdk iphonesimulator9.3 -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.3' clean build GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES | xcpretty
-sleep 10
-set -o pipefail && xcodebuild -scheme DronePan -sdk iphonesimulator9.3 -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.3' test GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES | xcpretty
