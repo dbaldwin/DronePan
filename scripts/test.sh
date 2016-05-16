d5007b7992916103080c1373dc6813bbb3bef91c
Chris Searle <chris@chrissearle.org>
2016-05-16 08:18:42 +0200

    Fix coverage script flag

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-xcodebuild -scheme DronePan -sdk iphonesimulator9.3 -destination "platform=iOS Simulator,name=iPhone 6,OS=9.3" build test GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES
+xcodebuild -scheme DronePan -sdk iphonesimulator9.3 -destination "platform=iOS Simulator,name=iPhone 6,OS=9.3" -enableCodeCoverage YES test
